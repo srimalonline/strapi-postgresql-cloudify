9755eda586faaa47ba22225f8489d69b4bc0b931
srimalonline <dilankasrimal23@gmail.com>
2022-12-27 01:12:38 +0530

    creating siteheader

---
+++ frontend/src/components/SiteHeader.js
@@ -0,0 +1,7 @@
+export default function SiteHeader() {
+    return (
+        <div className="site-header">
+            <Link to="/"><h1>Review Blog</h1></Link>
+        </div>
+    )
+}
---
+++ frontend/src/pages/ReviewDetails.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function ReviewDetails() {
+  return (
+    <div>ReviewDetails</div>
+  )
+}
