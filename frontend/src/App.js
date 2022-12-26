cc3ce0d72ee5e6190debcf516c2ef870616426aa
srimalonline <dilankasrimal23@gmail.com>
2022-12-27 01:16:33 +0530

    adding react router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,11 @@
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+//page and layout imports
+import Homepage from "./pages/Homepage";
+import ReviewDetails from "./pages/ReviewDetails";
+import Category from "./pages/Category";
+import SiteHeader from "./components/SiteHeader";
+
 function App() {
   return (
     <div className="App">
